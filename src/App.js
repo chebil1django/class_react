c8ede179f18aeeb0b638a4b3b10dd4032551ef7f
med_django <59399349+chebil1django@users.noreply.github.com>
2024-06-30 11:37:38 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Profil ',
+      bio: ' Experience with react',
+      imgSrc: "https://th.bing.com/th/id/OIP.F8N47NFCKAaMf97INBReHQAAAA?rs=1&pid=ImgDetMain",
+      profession: 'Engineer'
+    },
+    show: false,
+    intervalId: null,
+    mountedTime: null,
+  };
+
+  componentDidMount() {
+ 
+    const mountedTime = new Date();
+    this.setState({ mountedTime });
+
+
+    const intervalId = setInterval(() => {
+      this.setState({ mountedTime: new Date() });
+    }, 1000);
+
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+
+    clearInterval(this.state.intervalId);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, mountedTime } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Person Profile</h1>
+        <button onClick={this.toggleShow}>Profil à afficher</button>
+
+        {show && (
+          <div className="profile">
+            <img src={imgSrc} alt="Person" />
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <p>Prodession: {profession}</p>
+          </div>
+        )}
+
+       {/* <p>Time since component mounted: {Math.round((new Date() - mountedTime) / 1000)} seconds</p> */}
+      </div>
+    );
+  }
+}
+
+export default App;
